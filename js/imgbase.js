e1bbec3f903cfc31d86a2ca122306c4b6245f669
sehee.chang <sehee@atomrigs.io>
2022-05-26 02:06:33 +0900

    -. img info

---
+++ js/imgbase.js
@@ -0,0 +1,47 @@
+//header add class fixed
+$(function () {
+  let header = document.querySelector("#header");
+  let headerHeight = header.offsetHeight;
+
+  window.onscroll = function () {
+    let windowTop = window.scrollY;
+    if (windowTop >= headerHeight) {
+      header.classList.add("fixed");
+    } else {
+      header.classList.remove("fixed");
+    }
+  };
+});
+
+window.addEventListener("load", function () {
+  start();
+});
+
+function start() {
+  let imgList = img_list;
+  console.log(imgList.length);
+  let imgInfo_block = document.getElementById("img_list").innerHTML;
+
+  for (let i = 0; i < imgList.length; i++) {
+    // console.log("---------------------------------------");
+    // console.log("img " + i + " :  " + imgList[i].id);
+    // console.log("img " + i + " :  " + imgList[i].kName);
+    // console.log("img " + i + " :  " + imgList[i].eName);
+    // console.log("img " + i + " :  " + imgList[i].cName);
+    let htmlcontent = "";
+    let card = document.createElement("li");
+    htmlcontent = htmlcontent + '<a href="./img_detail.html?id=' + imgList[i].id + '" target="_blank">';
+    if (i < 9) {
+      htmlcontent = htmlcontent + '<div class="imgArea"><img src="../img/introduction/00' + imgList[i].id + '.jpg" /></div>';
+    } else {
+      htmlcontent = htmlcontent + '<div class="imgArea"><img src="../img/introduction/0' + imgList[i].id + '.jpg" /></div>';
+    }
+    htmlcontent = htmlcontent + '<p class="imgCount">No. ' + imgList[i].id + "</p>";
+    htmlcontent = htmlcontent + '<p class="eName">' + imgList[i].eName + "</p>";
+    htmlcontent = htmlcontent + '<p class="kName">' + imgList[i].kName + "</p>";
+    htmlcontent = htmlcontent + '<p class="cName">' + imgList[i].cName + "</p>";
+    htmlcontent = htmlcontent + "</a>";
+    card.innerHTML = htmlcontent;
+    document.getElementById("img_list").appendChild(card);
+  }
+}
