8a1fe60505109ff0bdc549468c76edf4330c9b61
Ellen Kim <67787451+chaelimKIM@users.noreply.github.com>
2022-07-04 10:52:16 +0900

    Revert "Update for SEO"
    This reverts commit d29facd1a770a7ddb0ae08d146eadce076752fb4.

--- js/locales.js
+++ js/locales.js
@@ -5,26 +5,26 @@ function changeLang(_lang){
     let home = url.indexOf("index");
     if (home > 0) {
       if(cur_lang==="ko" && _lang==="en"){
-        location.href="../Kansong_Metaverse_Museum.html";
+        location.href="../index.html";
       }else{
-        location.href="../Kansong_Metaverse_Museum_ko.html";
+        location.href="../index_ko.html";
       }
     } else{
       let kopage = url.indexOf("ko");
       let enpage = url.indexOf("en");
       if(kopage<0 && enpage<0){
         if(cur_lang==="ko" && _lang==="en"){
-          location.href="../Kansong_Metaverse_Museum.html";
+          location.href="../index.html";
         }else{
-          location.href="../Kansong_Metaverse_Museum_ko.html";
+          location.href="../index_ko.html";
         }
       }
     }
 
     // if(_lang === "ko"){
-    //   location.href="../Kansong_Metaverse_Museum_ko.html";
+    //   location.href="../index_ko.html";
     // }else{
-    //   location.href="../Kansong_Metaverse_Museum.html";
+    //   location.href="../index.html";
     // }
   }
 
@@ -32,14 +32,14 @@ function changeLanguage(change_to_this){
     let url = window.location.href;
     let home = url.indexOf("index");
 
-    if( home > 0 ) {location.href="../Kansong_Metaverse_Museum.html";}
+    if( home > 0 ) {location.href="../index.html";}
 
     if( change_to_this==="en"){
-        location.href="../Kansong_Metaverse_Museum.html";
+        location.href="../index.html";
 
     }
     else if(change_to_this === "ko"){
-        location.href="../Kansong_Metaverse_Museum_ko.html";
+        location.href="../index_ko.html";
 
     }
 
